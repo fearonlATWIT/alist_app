32d1d22ce1a0d981928bbed224ed1be42fccc7c2
Lola Fearon <fearonl@wit.edu>
2024-08-04 10:31:48 -0400

    Update settings.py

--- alistprj/settings.py
+++ alistprj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+#3o)pzk2dmmip+e5uwxpxv56d50o&2@%ao9)h=1oqf71giynj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1,' '142.93.63.99']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '142.93.63.99']
 
 
 # Application definition
